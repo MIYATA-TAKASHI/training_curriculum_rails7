6c4cda20b13a6322fd2d0c77f6d905e0b4ecf80c
MIYATA-TAKASHI <kulando000@gmail.com>
2023-08-03 18:56:51 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -18,24 +18,32 @@ class CalendarsController < ApplicationController
     params.require(:plan).permit(:date, :plan)
   end
 
+  require 'date'  # 追加
+
   def getWeek
     wdays = ['(日)', '(月)', '(火)', '(水)', '(木)', '(金)', '(土)']
-
+  
     @todays_date = Date.today
     @week_days = []
-
+  
     plans = Plan.where(date: @todays_date..@todays_date + 6)
-
+  
     7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans }
+  
+      # 曜日の添字を計算し、7以上になる場合は7を引いてループさせる
+      wday_index = (@todays_date.wday + x) % 7
+  
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, wday: wdays[wday_index], plans: today_plans }
       @week_days.push(days)
     end
-
+  
     @week_days
   end
+  
+
 
 end
